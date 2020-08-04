b9c2cf7e22a9343257b0d16867f2a2a199d253c7
Clemens Hug <clemens.hug@gmail.com>
2020-08-04 16:42:17 -0400

    Selecting columns for display works with human readable titles

--- modules/components/comp_tablevars.R
+++ modules/components/comp_tablevars.R
@@ -4,9 +4,9 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_tablevars_ui <- function(id){
   ns <- NS(id)
   tagList(
@@ -14,40 +14,37 @@ mod_tablevars_ui <- function(id){
       id = ns("tablevars"),
       inline = TRUE,
       placeholder = "Make a selection",
-      choices = COLUMNS,
+      choices = COLUMN_TITLE_MAP,
+      values = names(COLUMN_TITLE_MAP),
       selected = DEFAULT_COLUMNS
     ) %>%
       active("grey") %>%
       shadow()
   )
 }
-    
+
 #' tablevars Server Function
 #'
-#' @noRd 
+#' @noRd
 mod_tablevars_server <- function(input, output, session, r){
   ns <- session$ns
-  
-  
-  observeEvent(input$tablevars, {
 
-    if(is.null(input$tablevars)) r$tablevars <- DEFAULT_COLUMNS
-    if(!is.null(input$tablevars)){
-      r$tablevars <- COLUMNS[COLUMNS%in%input$tablevars] %>% names()
-      if("Commercial assays available" %in% input$tablevars){
-        r$tablevars <- c(r$tablevars, TWO_COLUMNS)
-        r$tablevars <- r$tablevars[r$tablevars!="TWO_COLUMNS"]
-      }
-        
-      
+  observeEvent(input$tablevars, {
+    tablevars <- input$tablevars %||% DEFAULT_COLUMNS
+    multivars <- intersect(tablevars, names(MULTISELECT_COLUMNS))
+    if (length(multivars) > 0) {
+      tablevars <- tablevars %>%
+        setdiff(multivars) %>%
+        c(MULTISELECT_COLUMNS[multivars])
     }
+    r$tablevars <- tablevars
   }, ignoreNULL = FALSE)
- 
+
 }
-    
+
 ## To be copied in the UI
 # mod_tablevars_ui("tablevars_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_tablevars_server, "tablevars_ui_1")
- 
+
