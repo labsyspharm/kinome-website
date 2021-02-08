ac1ece297741a3e8fc320077a6778d4f09d32068
Clemens Hug <clemens.hug@gmail.com>
2021-02-08 15:34:34 -0500

    Fix order and style of column selection buttons

--- modules/components/comp_tablevars.R
+++ modules/components/comp_tablevars.R
@@ -10,15 +10,6 @@
 mod_tablevars_ui <- function(id){
   ns <- NS(id)
 
-  # tagList(
-  #   checkbarInput(
-  #     id = ns("tablevars"),
-  #     choices = COLUMN_TITLE_MAP,
-  #     values = names(COLUMN_TITLE_MAP),
-  #     selected = DEFAULT_COLUMNS,
-  #     class = "btn-group-primary"
-  #   )
-  # )
   groupedCheckbarInput(
     id = ns("tablevars"),
     choices = COLUMN_SPECS,
@@ -29,40 +20,31 @@ mod_tablevars_ui <- function(id){
     col_group = "button_group"
   )
 }
-#
-# groupedCheckbarInput <- function(
-#   ...,
-#   choices,
-#   col_id,
-#   col_title,
-#   col_description,
-#   col_group
-# ) {
-#   args <- rlang::list2(...)
-#   button_groups <- choices %>%
-#     group_by(!!sym(col_group)) %>%
-#     group_map(
-#       ~{
-#         cb <- checkbarInput(
-#           id = paste(args[["id"]], .y[[col_group]], sep = "_"),
-#           choices = .x[[col_title]],
-#           values = .x[[col_id]],
-#           selected = args[["selected"]][args[["selected"]] %in% .x[[col_id]]],
-#           class = paste(args[["class"]], "mb-1")
-#         )
-#         attr(cb, "class") <- c("shiny.tag")
-#         cb
-#       }
-#     )
-#   # browser()
-#   tags$div(
-#     button_groups,
-#     id = args[["id"]],
-#     class = "yonder_checkbar yonder_input shiny.tag btn-toolbar flex flex-wrap",
-#     role = "toolbar",
-#     `aria-label` = "Toolbar with button groups"
-#   )
-# }
+
+map_checkbuttons_tooltip <- function (choices, values, selected, tooltips) {
+  if (is.null(choices) && is.null(values)) {
+    return(NULL)
+  }
+  selected <- values %in% selected
+  Map(choice = choices, value = values, select = selected, tooltip = tooltips,
+      function(choice, value, select, tooltip) {
+        tags$label(
+          class = yonder:::str_collate(
+            "btn btn-primary",
+            if (select) "active"
+          ),
+          `data-toggle` = "tooltip",
+          title = tooltip,
+          tags$input(
+            type = "checkbox",
+            autocomplete = "off",
+            value = value,
+            checked = if (select) NA
+          ),
+          choice
+        )
+      })
+}
 
 groupedCheckbarInput <- function (
   ...,
@@ -75,38 +57,36 @@ groupedCheckbarInput <- function (
   col_group
 )
 {
+  col_group_sym <- sym(col_group)
   button_groups <- choices %>%
-    group_by(!!sym(col_group)) %>%
+    mutate(
+      across(!!col_group_sym, forcats::fct_inorder)
+    ) %>%
+    group_by(!!col_group_sym) %>%
     group_map(
       ~{
-        yonder:::map_checkbuttons(
+        map_checkbuttons_tooltip(
           choices = .x[[col_title]],
           values = .x[[col_id]],
-          selected = selected[selected %in% .x[[col_id]]]
+          selected = selected[selected %in% .x[[col_id]]],
+          tooltips = .x[[col_description]]
         ) %>%
           tags$div(
             class = "btn-group btn-group-toggle btn-group-primary mb-2"
           )
       }
     )
-  tag <- yonder:::dep_attach({
-    # checkboxes <- map_checkbuttons(choices, values, selected)
+  yonder:::dep_attach({
     tags$div(class = "yonder-checkbar btn-toolbar flex flex-wrap",
              id = id, `data-toggle` = "buttons", button_groups, ...)
-  })
-  yonder:::s3_class_add(tag, c("yonder_checkbar", "yonder_input"))
+  }) %>%
+    yonder:::s3_class_add(c("yonder_checkbar", "yonder_input")) %>%
+    tagList(
+      tags$script(HTML(
+        "$('[data-toggle=\"tooltip\"]').tooltip();"
+      ))
+    )
 }
-
-x <- groupedCheckbarInput(
-  id = "asdf",
-  class = "btn-group-primary",
-  choices = COLUMN_SPECS,
-  col_id = "column_id",
-  col_title = "column_title",
-  col_description = "column_description",
-  col_group = "button_group",
-  selected = DEFAULT_COLUMNS
-)
 
 #' tablevars Server Function
 #'
