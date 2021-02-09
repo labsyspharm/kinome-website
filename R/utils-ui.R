d9bd819ada17bbf550a4df72fef4b88ecc894604
Clemens Hug <clemens.hug@gmail.com>
2021-02-09 09:08:42 -0500

    Filter heading font size smaller

--- R/utils-ui.R
+++ R/utils-ui.R
@@ -3,16 +3,14 @@ collapse_wrapper <- function(internal_html, open, collapseid, title, description
 
   div(class = "panel-group",
       div(class = "panel-heading",
-          h3(
-            class = "panel-title",
-            tags$a(
-              `data-toggle` = "collapse",
-              `aria-expanded` = ifelse(open, "true", "false"),
-              href = paste0("#", collapseid),
-              paste(title),
-              icon("chevron-right")
-            )
-          )),
+        tags$a(
+          `data-toggle` = "collapse",
+          `aria-expanded` = ifelse(open, "true", "false"),
+          href = paste0("#", collapseid),
+          paste(title),
+          icon("chevron-right")
+        )
+      ),
       div(
         id = collapseid,
         class = glue::glue("panel-collapse collapse {ifelse(open, 'in', '')}"),
